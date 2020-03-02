b95df6c048d0a32fa4f2e1955b10aacdc4d1300e
Gabrielle Nelson <gabrielle.a.nelson@wmich.edu>
2020-03-02 16:17:43 -0500

    ADD GOOOGLE MAP

---
+++ Home.js
@@ -0,0 +1,9 @@
+var mapOptions = {
+	center: new google.maps.LatLng(0,0),
+	panControl: true, zoomControl: true, mapTypeControl: false, scaleControl: true,
+	streetViewControl: true, rotateControl: true
+}
+	
+
+var map = new google.maps.Map(document.getElementsById("nearbymap");, mapOptions);
+
